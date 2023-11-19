d61079a8e69bfe7ee150ac4c2ada68348f04e75b
topmbr <centerm9911@gmail.com>
2023-11-19 09:39:56 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp73/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleApp73
+{
+    class Program
+    {
+        static Mutex mutex = new Mutex();
+        static int count = 20;
+        static int count1 = 10;
+
+        static void Main()
+        {
+            Thread firstThread = new Thread(CountUp);
+            Thread secondThread = new Thread(CountDown);
+
+            firstThread.Start();
+            secondThread.Start();
+
+            firstThread.Join();
+            secondThread.Join();
+        }
+
+        static void CountUp()
+        {
+            mutex.WaitOne();
+
+            for (int i = 0; i <= count; i++)
+            {
+                Console.WriteLine($"Thread 1: {i}");
+                Thread.Sleep(100);
+            }
+
+            mutex.ReleaseMutex();
+        }
+
+        static void CountDown()
+        {
+            mutex.WaitOne();
+
+            for (int i = count1; i >= 0; i--)
+            {
+                Console.WriteLine($"Thread 2: {i}");
+                Thread.Sleep(100);
+            }
+
+            mutex.ReleaseMutex();
+        }
+    }
+}
